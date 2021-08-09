18a4ccad329dd1ab18f1e6b50fb7092d92eb2bd5
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-09 17:19:28 +0200

    Add involment api class

--- src/classes/data.js
+++ src/classes/data.js
@@ -4,7 +4,7 @@ export default class Data {
   }
 
   getData = async () => {
-    const response = await fetch('https://api.tvmaze.com/shows');
+    const response = await fetch('https://api.tvmaze.com/shows?page=9');
 
     const data = await response.json();
     return data;
