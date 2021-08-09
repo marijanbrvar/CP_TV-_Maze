7db20776e8608cd1ea277703835de5b40b8d0c13
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-08-09 18:18:49 +0300

    Add styling for items added in Dom

--- src/index.js
+++ src/index.js
@@ -3,4 +3,27 @@ import Data from './classes/data';
 
 const data = new Data();
 data.init();
+
 console.log(data.list);
+
+const main = document.querySelector('MAIN');
+
+const displayItems = (data) => {
+  const newdata = data.map((item) => {
+    const { id, image } = item;
+    return `<div class ='image'>
+    <img src='${image.medium}' alt='image'>
+    <div class='info'>
+    <p class ='title'>Space ${id}</p>
+    <div class='likes>
+    <span class="material-icons-sharp">
+favorite_border</span>
+<span class='like'>likes</span>
+</div>
+</div>
+</div>`;
+  }).join('');
+  main.innerHTML = newdata;
+};
+
+displayItems(data.list);
