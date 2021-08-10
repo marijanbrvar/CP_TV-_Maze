2205413ae14ecd185baa0894567c438445b5df76
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-08-10 22:17:04 +0300

    Update getLikes function

--- src/classes/involvement.js
+++ src/classes/involvement.js
@@ -1,8 +1,8 @@
 export default class Involvement {
   constructor() {
-    this.baseUrl =
-      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    this.baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
     this.app_id = '5C88SbpqPP3Eb5tpLRl0%';
+    this.likeData = [];
   }
 
   async getLikes() {
