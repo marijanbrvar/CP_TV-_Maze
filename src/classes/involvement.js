a048553526400b8913ee850ee70ad34153cd529d
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-08-10 21:08:44 +0300

    Create postLikes method in Involvement class

--- src/classes/involvement.js
+++ src/classes/involvement.js
@@ -3,4 +3,18 @@ export default class Involvement {
     this.baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
     this.app_id = '5C88SbpqPP3Eb5tpLRl0%';
   }
+
+  postLikes = (itemid) => {
+    const params = { item_id: `${itemid}` };
+    const URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5C88SbpqPP3Eb5tpLRl0/likes/';
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers: {
+        'Content-type': 'application/json',
+      }
+        .then((response) => response.json())
+        .then((data) => console.log('Sucess:', data)),
+    });
+  };
 }
