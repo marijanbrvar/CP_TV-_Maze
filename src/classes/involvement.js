4d59aaf7ce489e11bd56f767fe86b1b2e4f63176
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-10 09:57:55 +0200

    Add get data method to involvment class

--- src/classes/involvement.js
+++ src/classes/involvement.js
@@ -1,6 +1,12 @@
 export default class Involvement {
   constructor() {
-    this.baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
     this.app_id = '5C88SbpqPP3Eb5tpLRl0%';
+  }
+
+  async getData(endpoint) {
+    const res = await fetch(`${this.url}/apps/${this.app_id}/${endpoint}/`, { method: 'GET' });
+    const data = await res.json();
+    return data;
   }
 }
