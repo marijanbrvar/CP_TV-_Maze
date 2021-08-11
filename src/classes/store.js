1c63ad03c9839fc195105ba9bd5d00baf1ac3e83
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-08-11 20:11:31 +0300

    Create the getLikes method

--- src/classes/store.js
+++ src/classes/store.js
@@ -4,7 +4,8 @@ import Api from './api';
 export default class Store {
   constructor() {
     this.showsUrl = 'https://api.tvmaze.com';
-    this.involvementUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    this.involvementUrl =
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
     this.appId = '5C88SbpqPP3Eb5tpLRl0';
     this.shows = [];
     this.commments = [];
@@ -18,12 +19,19 @@ export default class Store {
   }
 
   async postComment(body) {
-    const res = await this.api.post(this.involvementUrl, `apps/${this.appId}/comments`, JSON.stringify(body));
+    const res = await this.api.post(
+      this.involvementUrl,
+      `apps/${this.appId}/comments`,
+      JSON.stringify(body)
+    );
     return res;
   }
 
   async getComment(id) {
-    const res = await this.api.get(this.involvementUrl, `apps/${this.appId}/comments?item_id=${id}`);
+    const res = await this.api.get(
+      this.involvementUrl,
+      `apps/${this.appId}/comments?item_id=${id}`
+    );
     if (res.length !== 0) {
       this.commments = res;
       return res;
@@ -34,5 +42,19 @@ export default class Store {
   getInvolvement = async (endpoint) => {
     const data = await this.involvment.getData(endpoint);
     return data;
+  };
+
+  async getLikes() {
+    const res = await this.api.get(
+      this.involvementUrl,
+      `apps/${this.appId}/likes/`
+    );
+    // eslint-disable-next-line max-len
+    const merge = this.shows.map((show) => ({
+      ...show,
+      ...res.find((like) => parseInt(like.item_id, 10) === show.id),
+    }));
+    this.shows = merge;
+    return res;
   }
 }
