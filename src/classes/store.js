001dde69297e98812b3f5d4da9633f122f72283a
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-11 15:15:13 +0200

    Fix store methods to reflect API class

--- src/classes/store.js
+++ src/classes/store.js
@@ -1,17 +1,34 @@
+/* eslint-disable class-methods-use-this */
 import Api from './api';
-import Involvement from './involvement';
 
 export default class Store {
   constructor() {
+    this.showsUrl = 'https://api.tvmaze.com';
+    this.involvementUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    this.appId = '5C88SbpqPP3Eb5tpLRl0';
     this.shows = [];
+    this.commments = [];
     this.api = new Api();
-    this.involvment = new Involvement();
   }
 
-  async getData() {
-    const data = await this.api.getShows();
+  async getShows() {
+    const data = await this.api.get(this.showsUrl, '/shows');
 
-    this.shows = data;
+    this.shows = data.slice(0, 12);
+  }
+
+  async postComment(body) {
+    const res = await this.api.post(this.involvementUrl, `apps/${this.appId}/comments`, JSON.stringify(body));
+    return res;
+  }
+
+  async getComment(id) {
+    const res = await this.api.get(this.involvementUrl, `apps/${this.appId}/comments?item_id=${id}`);
+    if (res.length !== 0) {
+      this.commments = res;
+      return res;
+    }
+    return [];
   }
 
   getInvolvement = async (endpoint) => {
