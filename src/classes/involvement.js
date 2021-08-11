ebf0025d975cdaf4e410284277219c819256b6c0
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-08-11 10:54:24 +0300

    Update get method

--- src/classes/involvement.js
+++ src/classes/involvement.js
@@ -1,22 +1,16 @@
 export default class Involvement {
   constructor() {
     this.baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
-    this.app_id = '5C88SbpqPP3Eb5tpLRl0%';
-    this.likeData = [];
+    this.app_id = '5C88SbpqPP3Eb5tpLRl0';
+  }
+
+  async initialize() {
+    await this.getLikes();
   }
 
   async getLikes() {
-    const URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5C88SbpqPP3Eb5tpLRl0/likes';
-    await fetch(URL, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        this.likeData = json;
-        console.log(json);
-      });
+    const response = await fetch(`${this.baseUrl}/apps/${this.app_id}/likes`, { method: 'GET' });
+    const likeData = await response.json();
+    return likeData;
   }
 }
